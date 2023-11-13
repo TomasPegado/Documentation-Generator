cc06b50778ecb6bbffa56efc1c6087f375e433ed
TomasPegado <lenzitomas00@gmail.com>
2023-11-12 21:31:41 -0300

    Extrair Modulos e Imports

--- entidades/dicionario/Gera_Dicionario.py
+++ entidades/dicionario/Gera_Dicionario.py
@@ -4,17 +4,18 @@ __all__ = ["geraDicionario"]
 
 def geraDicionario(conteudo):
     
-    dicionario = dict()
+    dicionario = {'descrição': '', 'modulos': {}}
     buscaDescricaoDoProjeto(conteudo, dicionario)
+    buscaModulos(conteudo, dicionario)
 
     return dicionario
 
 def buscaDescricaoDoProjeto(conteudo, dicionario):
 
     # Definir o padrão de regex
-    padrao = r'Descrição do Módulo:(.*?)"""'
+    padrao = r'Descrição do Projeto:(.*?)"""'
 
-    # Buscar pela descrição do módulo
+    # Buscar pela descrição do projeto
     match = re.search(padrao, conteudo, re.DOTALL)
 
     if match:
@@ -22,4 +23,53 @@ def buscaDescricaoDoProjeto(conteudo, dicionario):
         dicionario['descrição'] = match
         return match
     else:
-        return "Descrição do módulo não encontrada."
+        return "Descrição do projeto não encontrada."
+
+def buscaImports(conteudo):
+
+    # Definir o padrão de regex
+    padrao = r'^import\s+([^\n]+)'
+
+    # Encontrar todas as correspondências para 'import'
+    matches = re.findall(padrao, conteudo, re.MULTILINE)
+    print(matches)
+
+    return matches
+
+def buscaModulos(conteudo, dicionario):
+
+    # Definir o padrão de regex
+    padrao = r'Modulo:(.*?)"""'
+
+    # Buscar pelo modulo
+    match = re.search(padrao, conteudo, re.DOTALL)
+
+    if match:
+        match = match.group(1).strip()  # Retorna o texto capturado, removendo espaços extras
+        dicionario['modulos'][match] = dict()
+
+        # Buscar imports dentro do modulo
+        padraoModulo = rf"Modulo:(.*?)Modulo:"
+        conteudoModulo = re.search(padraoModulo, conteudo, re.DOTALL)
+        conteudoModulo = conteudoModulo.group(1).strip()
+        # print(conteudoModulo)
+        matches = buscaImports(conteudoModulo)
+        dicionario['modulos'][match]['imports'] = matches
+
+
+        proximo = rf"{match}(.*)$" # Busca o modulo seguinte
+        conteudo_restante = re.search(proximo, conteudo, re.DOTALL)
+        conteudo_restante = conteudo_restante.group(1)
+        proximoModulo = re.search(padrao, conteudo_restante, re.DOTALL)
+        print(proximoModulo)
+
+        try:
+            capturado = proximoModulo.group(1) # Obtém a parte capturada pelo (.*)
+            print(capturado)
+            return buscaModulos(capturado, dicionario)
+        
+        except AttributeError:
+            print("Nenhum conteudo restante.")
+            return "Nenhum conteudo restante."
+    else:
+        return "Nenhum modulo foi encontrado."
--- entidades/dicionario/Teste.py
+++ entidades/dicionario/Teste.py
@@ -1,11 +1,10 @@
 import unittest
-# from entidades.dicionario import *
 import Gera_Dicionario as gd
 
 
-class TestExtrairDescricaoModulo(unittest.TestCase):
+class TestExtrairDescricaoProjeto(unittest.TestCase):
     def test_01_busca_descricao_ok_condicao_retorno(self):
-        print("Caso de Teste 01 - Buscar com sucesso")
+        print("Caso de Teste 01 - Buscar descrição com sucesso")
 
         with open("/home/tomas/PUC/Modular/arquivoTeste.txt", 'r') as arquivo:
             conteudo = arquivo.read()
@@ -14,7 +13,21 @@ class TestExtrairDescricaoModulo(unittest.TestCase):
 
         retorno_esperado = dicionario['descrição']
 
-        self.assertEqual(retorno_esperado, "Esse módulo foi feito para gerar um dicionario a partir de um arquivo")
+        self.assertEqual(retorno_esperado, "Esse projeto foi feito para gerar um dicionario a partir de um arquivo")
+    
+
+class TestExtrairModulo(unittest.TestCase):
+    def test_01_busca_modulo_ok_condicao_retorno(self):
+        print("Caso de Teste 01 - Buscar Modulo com sucesso")
+
+        with open("/home/tomas/PUC/Modular/arquivoTeste.txt", 'r') as arquivo:
+            conteudo = arquivo.read()
+
+        dicionario = gd.geraDicionario(conteudo)
+
+        retorno_esperado = dicionario['modulos']
+
+        self.assertEqual(retorno_esperado, {"Gera_Dicionario": {}, "Documentacao": {}})
 
 if __name__ == '__main__':
     unittest.main()
