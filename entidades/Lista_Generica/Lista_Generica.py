3cc1bb49343124a06a5a60d36a2ab1b8d5addef6
em1ru <143016996+em1ru@users.noreply.github.com>
2023-11-17 19:36:49 -0300

    Cria diretório Lista_Generica e adiciona arquivo de teste

---
+++ entidades/Lista_Generica/Lista_Generica.py
@@ -0,0 +1,28 @@
+
+def insereElemento(dicionario, dado, chave, posicao=None):
+    if chave not in dicionario:
+        dicionario[chave] = []
+    if posicao is None:
+        dicionario[chave].append(dado)
+    else:
+        dicionario[chave].insert(posicao, dado)
+
+def removeElemento(dicionario, dado, chave, posicao=None):
+    if chave in dicionario:
+        if posicao is None:
+            dicionario[chave].remove(dado)
+        else:
+            dicionario[chave].pop(posicao)
+
+def buscaElemento(dado, posicao, chave, dicionario):
+    if chave in dicionario:
+        if posicao is None:
+            return dado in dicionario[chave]
+        else:
+            return dicionario[chave][posicao] == dado
+    return False
+
+def insereChave(dicionario, chave):
+    if chave not in dicionario:
+        dicionario[chave] = []
+    return dicionario
