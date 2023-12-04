d31efb4b13a05191a91db700eab31e8a1d4667b9
TomasPegado <lenzitomas00@gmail.com>
2023-12-04 11:15:30 -0300

    Teste Gerenciador

--- teste_gerador.py
+++ teste_gerador.py
@@ -1,29 +1,16 @@
 import unittest
-import entidades.Gerador.gerador as ge
+import entidades.Gerenciador.gerenciador as ge
 
 class TestExtraiConteudoDaPasta(unittest.TestCase):
 
     def test_01_extrai_arquivos_python(self):
         print("Caso de Teste 01 - Buscar arquivos com sucesso")
 
-        caminho = "/home/tomas/PUC/Modular/modulos"
+        caminho = "modulos"
 
         retorno_esperado = ge.pegaArquivos(caminho)
 
-        self.assertEqual(retorno_esperado, ["Lista_Generica.py", "Gera_Dicionario.py"])
-
-class TestGeraDicionario(unittest.TestCase):
-
-    def test_02_gera_dicionario_dados(self):
-        print("Caso de Teste 02 - Gerar Dicionario com todos os dados com sucesso")
-
-        caminho = "/home/tomas/PUC/Modular/modulos"
-
-        retorno_esperado = ge.generator(caminho)
-
-        dicionario = {'Lista_Generica': {'descrição': '', 'nome': '', 'imports': [], 'funcoes': {'insereElemento': {}, 'removeElemento': {}, 'buscaElemento': {}, 'insereChave': {}}, '_all_': []}, 'Gera_Dicionario': {'descrição': 'Esse projeto foi feito para gerar um dicionario a partir de um arquivo', 'nome': 'Gera_Dicionario', 'imports': ['regex as re'], 'funcoes': {'geraDicionario': {'descricao': 'Aqui esta a descrição da função geraDicionario\n\n    Parameters\n    ----------\n    conteudo : _type_\n        _description_\n\n    Returns\n    -------\n    _type_\n        _description_'}, 'buscaDescricaoDoModulo': {'descricao': 'Aqui esta a descrição da função geraDicionario\n\n    Parameters\n    ----------\n    conteudo : str\n        _description_\n    dicionario : dict\n        _description_\n\n    Returns\n    -------\n    _type_\n        _description_'}, 'buscaNome': {'descricao': '_summary_\n\n    Parameters\n    ----------\n    conteudo : str\n        _description_\n    dicionario : dict\n        _description_\n\n    Returns\n    -------\n    _type_\n        _description_'}, 'buscaImports': {'descricao': '_summary_\n\n    Parameters\n    ----------\n    conteudo : str\n        _description_\n    dicionario : dict\n        _description_'}, 'buscaDescricaoFuncao': {'descricao': '_summary_\n\n    Parameters\n    ----------\n    conteudo : str\n        _description_\n    dicionario : dict\n        _description_\n    funcao : str\n        _description_'}, 'buscaFuncoes': {'descricao': '_summary_\n\n    Parameters\n    ----------\n    conteudo : str\n        _description_\n    dicionario : dict\n        _description_'}, 'buscaAll': {'descricao': '_summary_\n\n    Parameters\n    ----------\n    conteudo : str\n        _description_\n    dicionario : dict\n        _description_'}}, '_all_': ['geraDicionario']}}
-
-        self.assertEqual(retorno_esperado, dicionario)
+        self.assertEqual(retorno_esperado, ['Formata_HTML.py','Lista_Generica.py','Gera_Dicionario.py','static','home.txt','Gerenciador.py'])
 
 
 if __name__ == '__main__':
